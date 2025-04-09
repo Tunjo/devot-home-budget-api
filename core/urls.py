3a254a4312cd2b46c0e81d66def394e2c1f546c6
Antun Franjin <antun.franjin@undabot.com>
2025-04-09 22:16:29 +0200

    feat: add swager & urls

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,47 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from drf_yasg.views import get_schema_view
+
+from rest_framework import permissions
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Devot-home-budget-API",
+      default_version='v1',
+      description="Documentation for Devot-home-budget API",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="antun.franjin@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+swagger_urls = [
+    re_path(
+        r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-json'
+    ),
+    path(
+        'swagger/',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'
+    ),
+    path(
+        'redoc/',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'
+    ),
+]
 
 urlpatterns = [
+    path('', include(swagger_urls)),
     path('admin/', admin.site.urls),
     path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
