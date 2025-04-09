4f0ee2ddcab94e5a6cf9248ac960443089ad0f60
Antun Franjin <antun.franjin@undabot.com>
2025-04-09 22:28:39 +0200

    feat: make project use postgresql DB, Add backend.env file

--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -88,8 +89,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DJANGO_DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.getenv(
+            'DJANGO_DB_NAME',
+            os.path.join(BASE_DIR.parent, 'db.sqlite3')
+        ),
+        'USER': os.getenv('DJANGO_DB_USER', ''),
+        'PASSWORD': os.getenv('DJANGO_DB_PASSWORD', ''),
+        'HOST': os.getenv('DJANGO_DB_HOST', ''),
+        'PORT': os.getenv('DJANGO_DB_PORT', '')
     }
 }
 
