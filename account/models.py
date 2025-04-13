37a49464796ee6ab3646134a111ee5ffd070ab35
Antun Franjin <antun.franjin@undabot.com>
2025-04-13 07:48:03 +0200

    feat: BudgetHistory model and it's migrations added

--- account/models.py
+++ account/models.py
@@ -17,10 +17,30 @@ class AccountBudget(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s Budget: {self.budget}"
+    
+class BudgetHistory(models.Model):
+    INCOME = "income"
+    EXPENSE = "expense"
+    CHANGE_TYPES = [
+        (INCOME, "Income"),
+        (EXPENSE, "Expense"),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="budget_history")
+    change_type = models.CharField(max_length=10, choices=CHANGE_TYPES)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField(auto_now_add=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.change_type} - {self.amount} on {self.date}"
+
 
 @receiver(post_save, sender=User)
 def create_account_budget(instance, created, **kwargs):
     if created:
         AccountBudget.objects.create(user=instance)
+
+
 
 
